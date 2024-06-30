49c32ccbeea6fd8c69a0f2b8df6cf303efcea876
tayloh2 <tayloh2@mail.uc.edu>
2024-06-30 15:37:19 -0400

    GettingLoopy

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        for (int down = 1 ; down <= 5; down++) {
+            for (int side = 1 ; side <= 5; side++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
